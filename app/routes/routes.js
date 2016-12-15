ccdb15f4e148a9ee273cd5807d3ef8473dcd6396
Evgeny Shulkin <evgeny.shulkin@gmail.com>
2016-12-15 15:29:43 +1000

    Added query post route in backend

--- app/routes/routes.js
+++ app/routes/routes.js
@@ -17,5 +17,30 @@ module.exports = function(app) {
       if (err) res.send(err);
       res.json(user); // return new user
     })
-  })
+  });
+  // get JSON records of users who met certain query conditions
+  app.post("/query", function(req, res) {
+    // grab query params from request body
+    var lat = req.body.latitude;
+    var long = req.body.longitude;
+    var distance = req.body.distance;
+    // opens a generic mongoose query
+    var query = User.find({});
+    // include filter by max distance
+    if (distance) {
+      // notice MongoDB geospatial query!
+      query = query.where("location").near({
+        center: {type: "point", coordinates: [long, lat]},
+        maxDistance: distance, // meters
+        spherical: true // evaluate distances across the globe
+      });
+    }
+    // other queries will go here...
+    // execute query
+    query.exec(function(err, users) {
+      if (err) res.send(err);
+      // return all users who met criteria
+      res.json(users);
+    });
+  });
 };
