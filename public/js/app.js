e073bdaf6c53d94d398509c1f215167d48762e22
Evgeny Shulkin <evgeny.shulkin@gmail.com>
2016-12-15 14:36:00 +1000

    Added routes to views

--- public/js/app.js
+++ public/js/app.js
@@ -1,5 +1,18 @@
 angular.module("google.maps.app", [
   "add.ctrl", // controller for addUserForm
   "map.service", // corresponding data service
-  "geolocation" // HTML5 verified location
-]);
+  "geolocation", // HTML5 verified location
+  "ngRoute" // application routing
+])
+.config(function($routeProvider) {
+  // show the relevant view and controller when needed
+  $routeProvider
+  .when("/join", {
+    controller: "addCtrl",
+    templateUrl: "views/add-form.html"
+  })
+  .when("/find", {
+    templateUrl: "views/query-form.html"
+  })
+  .otherwise({redirectTo: "/join"});
+});
